8e1cd3e904c35fe7f0a56050f6816e03e665cab5
Richard Prasad <richarddprasad@gmail.com>
2018-07-30 18:10:21 -0700

    findObjects algorithm implemented

---
+++ algorithms/find_objects.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// const collection = [{ first: "Romeo", last: "Montague" },
+// { first: "Mercutio", last: null },
+// { first: "Tybalt", last: "Capulet" }
+// ];
+
+// const target = { last: "Capulet" };
+
+const collection = [{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }];
+const target = { "apple": 1, "cookie": 2 };
+
+// Returns all objects from a1 that have matching keys
+// and values with objects in a2
+function findObjects(a1, a2) {
+    console.log("a2 keys/values: ");
+    let a2Keys = Object.keys(a2);
+    let a2Values = Object.values(a2);
+    a2Keys.forEach(key => console.log(key + ": " + a2[key]));
+
+    let rv = [];
+    // Map over each element in the array
+    rv = a1.filter(obj => {
+
+
+        // Are all the key-value pairs in a2 present in a1?
+        let present = true;
+        a2Keys.forEach(key2 => {
+            if(obj[key2] !== a2[key2]) {
+                present = false;
+            }
+        });
+        return present;
+
+    });
+
+    console.log("Final result: ");
+    //console.log(rv);
+
+    return rv;
+}
+
+console.log(findObjects(collection, target));
+
+        // Iterate over each key from each object
+        // return (
+        //     Object.keys(obj).some(key => {
+
+        //         console.log("****************");
+        //         console.log("key: " + key + ", value: " + obj[key]);
+
+        //         console.log(key === a2Key);
+        //         return key === a2Key && obj[key] === a2Value;
+        //     }));
+
+        // let accept;
+        // accept = Object.keys(obj).reduce((accept, key) => {
+        //     return a2Keys.some(key2 => key === key2 && obj[key] === a2[key2]);
+        // }, false);
+        // return accept;
