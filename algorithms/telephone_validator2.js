11138fbdbdbeb5cde6e790a7b4dbc642984c5370
Richard Prasad <richarddprasad@gmail.com>
2018-08-07 09:55:37 -0700

    Minor code cleanup for freeCodeCamp Javascript certificate submission

--- algorithms/telephone_validator2.js
+++ algorithms/telephone_validator2.js
@@ -11,7 +11,8 @@ function telephoneCheck(str) {
     return isValid;
 }
 
-
+// Tests that should pass
+console.log("TESTS THAT SHOULD PASS");
 console.log(telephoneCheck("1 555-555-5555")); //should return true.
 console.log(telephoneCheck("1 (555) 555-5555")); //should return true.
 console.log(telephoneCheck("5555555555")); //should return true.
@@ -22,43 +23,23 @@ console.log(telephoneCheck("1 555 555 5555")); //should return true.
 console.log(telephoneCheck("1 456 789 4444")); //should return true.
 console.log("**********************************************");
 
+// Tests that should fail
+console.log("TESTS THAT SHOULD FAIL");
 console.log(telephoneCheck("555-5555")); //should return false.
 console.log(telephoneCheck("5555555")); //should return false.
-
-// // FAILED  
 console.log(telephoneCheck("1 555)555-5555")); //should return false.
-
-// console.log(telephoneCheck("1 555 555 5555")); //should return true.
-// console.log(telephoneCheck("1 456 789 4444")); //should return true.
-
-// console.log(telephoneCheck("123**&!!asdf#")); // return false.
-// console.log(telephoneCheck("55555555")); //should return false.
-
-// // FAILED
+console.log(telephoneCheck("123**&!!asdf#")); // return false.
+console.log(telephoneCheck("55555555")); //should return false.
 console.log(telephoneCheck("(6054756961)")); //should return false
-
-// // FAILED
-// console.log(telephoneCheck("2 (757) 622-7382")); //should return false.
-
-// // FAILED
-// console.log(telephoneCheck("0 (757) 622-7382")); // should return false.
-
-// // FAILED
-// console.log(telephoneCheck("-1 (757) 622-7382")); //should return false
-
-
-// console.log(telephoneCheck("2 757 622-7382")); //should return false.
-// console.log(telephoneCheck("10 (757) 622-7382")); //should return false.
-// console.log(telephoneCheck("27576227382")); //should return false.
-// console.log(telephoneCheck("(275)76227382")); //should return false.
-// console.log(telephoneCheck("2(757)6227382")); //should return false.
-// console.log(telephoneCheck("2(757)622-7382")); //should return false.
-
-// // FAILED
-// console.log(telephoneCheck("555)-555-5555")); //should return false.
-
-// // FAILED
-// console.log(telephoneCheck("(555-555-5555")); //should return false.
-
-
-// console.log(telephoneCheck("(555)5(55?)-5555")); //should return false.
+console.log(telephoneCheck("2 (757) 622-7382")); //should return false.
+console.log(telephoneCheck("0 (757) 622-7382")); // should return false.
+console.log(telephoneCheck("-1 (757) 622-7382")); //should return false
+console.log(telephoneCheck("2 757 622-7382")); //should return false.
+console.log(telephoneCheck("10 (757) 622-7382")); //should return false.
+console.log(telephoneCheck("27576227382")); //should return false.
+console.log(telephoneCheck("(275)76227382")); //should return false.
+console.log(telephoneCheck("2(757)6227382")); //should return false.
+console.log(telephoneCheck("2(757)622-7382")); //should return false.
+console.log(telephoneCheck("555)-555-5555")); //should return false.
+console.log(telephoneCheck("(555-555-5555")); //should return false.
+console.log(telephoneCheck("(555)5(55?)-5555")); //should return false.
