628f996d70b1e234707fd6cf0bdb87ce8ad46e0e
Richard Prasad <richarddprasad@gmail.com>
2018-08-16 15:40:01 -0700

    Working on quick_sort.js... not yet working

--- algorithms/quick_sort.js
+++ algorithms/quick_sort.js
@@ -1,5 +1,8 @@
 // TODO: Implement algorithm
 // TODO: Test algorithm
+// TODO: Sort and return copy instead of mutating original
+
+// Quick sort algorithm using ??? partition scheme
 
 // Helper function
 function swap(a, b, arr) {
@@ -8,6 +11,52 @@ function swap(a, b, arr) {
     arr[b] = t;
 }
 
-function quickSort(arr) {
+function quickSort(arr, lo, hi) {
+    let index;
 
+    if (arr.length > 1) {
+        index = partition(arr, lo, hi);
+        console.log("index: ", index);
+
+        if (lo < index - 1) {
+            quickSort(arr, lo, index - 1);
+        }
+
+        if (index < hi) {
+            quickSort(arr, index, hi);
+        }
+    }
+
+    return arr;
 }
+
+function partition(arr, lo, hi) {
+    let pivot = arr[Math.floor((hi + lo) / 2)];
+    let i = lo, j = hi;
+
+    while (i <= j) {
+
+        while (arr[i] < pivot) {
+            i++;
+        }
+
+        while (arr[j] > pivot) {
+            j--;
+        }
+
+        if (i <= j) {
+            swap(i, j, arr);
+            i++;
+            j--;
+        }
+
+        return i;
+    }
+}
+
+const unsorted = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+let sorted = quickSort(unsorted, 0, unsorted.length - 1);
+
+console.log("\nUnsorted: ", unsorted, " | size: ", unsorted.length);
+console.log("Sorted: ", sorted, " | size: ", sorted.length, "\n");
+
